709b140c750e23cf99c0be15e617495b410cb271
rmrotek <remek87@gmail.com>
2018-10-12 15:27:08 +0200

    Updates

--- precache-manifest.3e4516d8d15d3c0e2af9f881c8316685.js
+++ precache-manifest.3e4516d8d15d3c0e2af9f881c8316685.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/contact-list-react/static/js/runtime~main.c04afaa2.js"
   },
   {
-    "revision": "b24e25524c07ec9afe99",
-    "url": "/contact-list-react/static/js/main.b24e2552.chunk.js"
+    "revision": "f875992b963593d99437",
+    "url": "/contact-list-react/static/js/main.f875992b.chunk.js"
   },
   {
     "revision": "4fd91c74e51506835aa9",
     "url": "/contact-list-react/static/js/1.4fd91c74.chunk.js"
   },
   {
-    "revision": "b24e25524c07ec9afe99",
-    "url": "/contact-list-react/static/css/main.3219144e.chunk.css"
+    "revision": "f875992b963593d99437",
+    "url": "/contact-list-react/static/css/main.2c37d68b.chunk.css"
   },
   {
-    "revision": "e0aa21f4acac6399616624f5165009a4",
+    "revision": "53802cc2596779c5819f7605a7c6857e",
     "url": "/contact-list-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/contact-list-react/precache-manifest.ae3a77e2df58b24a7de87609cc9a1024.js"
+  "/contact-list-react/precache-manifest.3e4516d8d15d3c0e2af9f881c8316685.js"
 );
 
 workbox.clientsClaim();
