382e57dd6b91c03d31b92f29c344df32a3f6104a
rmrotek <remek87@gmail.com>
2018-10-12 11:15:21 +0200

    mini list of PH contacts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    contacts:
+      [
+        {
+          id: 1,
+          name: 'bob1',
+          surname: 'surnamebob1',
+
+        },
+        {
+          id: 2,
+          name: 'bob2',
+          surname: 'surnamebob2',
+
+        },
+        {
+          id: 3,
+          name: 'bob3',
+          surname: 'surnamebob3',
+
+        }
+      ]
+  }
+
   render() {
     return (
       <div>
-        Hello
+        {
+          this.state.contacts.map(
+            contact => (
+              <div key={contact.id}>
+                {contact.name} {contact.surname}
+              </div>
+            )
+          )
+        }
       </div>
     );
   }
