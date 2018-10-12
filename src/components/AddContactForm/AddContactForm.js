e1344a818107dc6a86adeef365a02cda00a3bd1d
rmrotek <remek87@gmail.com>
2018-10-12 11:52:13 +0200

    error msg displays

--- src/components/AddContactForm/AddContactForm.js
+++ src/components/AddContactForm/AddContactForm.js
@@ -4,25 +4,29 @@ import React, { Component } from 'react';
 class AddContactForm extends Component {
 
   state = {
-    contactName:'',
-    contactSurname:'',
-    needNameError: null,
-    needSurnameError: null
+    contactName: '',
+    contactSurname: '',
+    error: null,
 
   }
-  
+
   preventSubmit = event => {
     event.preventDefault();
-    
+    if (this.state.contactName === '' || this.state.contactSurame === '') {
+      this.setState({
+        error: new Error('Fields cant be empty')
+      })
+      return;
+    }
   }
 
-  handleNameChange= event => {
+  handleNameChange = event => {
     this.setState({
       contactName: event.target.value
     })
   }
 
-  handleSurnameChange= event => {
+  handleSurnameChange = event => {
     this.setState({
       contactSurname: event.target.value
     })
@@ -32,16 +36,17 @@ class AddContactForm extends Component {
     return (
       <form onSubmit={this.preventSubmit}>
         <p>Name
-        <input value={this.state.contactName} onChange={this.handleNameChange}/>
+        <input value={this.state.contactName} onChange={this.handleNameChange} />
 
         </p>
         <p>Surname
-        <input value={this.state.contactSurname} onChange={this.handleSurnameChange}/>
+        <input value={this.state.contactSurname} onChange={this.handleSurnameChange} />
 
         </p>
-
-
         <button>ADD</button>
+        {
+          this.state.error && <span>{this.state.error.message}</span>
+        }
       </form>
     )
   }
