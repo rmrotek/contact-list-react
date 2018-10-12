041f989b1851adc7e5d81e89a8a4e45cdff1137a
rmrotek <remek87@gmail.com>
2018-10-12 14:48:12 +0200

    Updates

---
+++ precache-manifest.ae3a77e2df58b24a7de87609cc9a1024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c04afaa237b5ab323ccc",
+    "url": "/contact-list-react/static/js/runtime~main.c04afaa2.js"
+  },
+  {
+    "revision": "b24e25524c07ec9afe99",
+    "url": "/contact-list-react/static/js/main.b24e2552.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/contact-list-react/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "b24e25524c07ec9afe99",
+    "url": "/contact-list-react/static/css/main.3219144e.chunk.css"
+  },
+  {
+    "revision": "e0aa21f4acac6399616624f5165009a4",
+    "url": "/contact-list-react/index.html"
+  }
+];
